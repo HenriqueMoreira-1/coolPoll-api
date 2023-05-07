1040e7ead3e34977e18550e103db7973a7148cf4
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-07 15:51:43 -0300

    feat: ensure signup controller returns an error if no name is providade

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@ export class SignUpController {
     handle(httpRequest: any): any {
         if (!httpRequest.body.name) {
             return {
-                statusCode: 400
+                statusCode: 400,
+                body: new Error('Missing param: name')
             }
         }
     }
