66b336ad17322df2af3d29d545fa37b009f1aa0d
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-07 15:58:59 -0300

    feat: ensure SignupController return an error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,16 @@
 export class SignUpController {
-    handle(httpRequest: any): any {
-        if (!httpRequest.body.name) {
-            return {
-                statusCode: 400,
-                body: new Error('Missing param: name')
-            }
-        }
+  handle (httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
     }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
+  }
 }
