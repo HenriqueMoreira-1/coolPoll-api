c073ce4b7a2e78a2a31695ac7b3f0240c3a46276
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-07 17:13:00 -0300

    refactor: simplify requiredFields code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,13 +4,12 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
     return badRequest(new MissingParamError('password'))
   }
 }
