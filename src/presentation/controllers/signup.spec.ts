8041a60c2d1a8c7c2852f2bedb11a02374fc0cc5
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-08 12:21:48 -0300

    test: move all error imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,6 @@
 import { type EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
