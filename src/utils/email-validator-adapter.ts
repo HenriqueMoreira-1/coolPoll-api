a70c9f3c9616d7a6cf5e34cee35dca174d7bd8f2
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-11 12:19:20 -0300

    refactor: change file name

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,5 +1,6 @@
 import { type EmailValidator } from '../presentation/protocols/email-validator'
 import validator from 'validator'
+
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
     return validator.isEmail(email)
