723fdd42c0bff6b47655efbacae06861368889f5
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-05-17 15:49:05 -0300

    feat: ensure request return json as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => { res.send() })
-    await request(app).post('/test_cors')
+    await request(app).get('/test_cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
